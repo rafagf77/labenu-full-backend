2371ba440edf2b18c46b3f719502f25458f2a206
rafae <rafaelgf@gmail.com>
2021-02-22 14:22:16 -0300

    Template

---
+++ src/data/UserDatabase.ts
@@ -0,0 +1,74 @@
+import BaseDataBase from "./BaseDatabase";
+import { User } from "../model/User";
+
+export class UserDatabase extends BaseDataBase {
+
+   protected tableName: string = "FullStack_user";
+
+   private toModel(dbModel?: any): User | undefined {
+      return (
+         dbModel &&
+         new User(
+            dbModel.id,
+            dbModel.name,
+            dbModel.email,
+            dbModel.nickname,
+            dbModel.password
+         )
+      );
+   }
+
+   public async createUser(user: User): Promise<void> {
+      try {
+         await BaseDataBase.connection.raw(`
+            INSERT INTO ${this.tableName} (id, name, email, password, role)
+            VALUES (
+            '${user.getId()}', 
+            '${user.getName()}', 
+            '${user.getEmail()}',
+            '${user.getNickname()}',
+            '${user.getPassword()}',
+            )`
+         );
+      } catch (error) {
+         throw new Error(error.sqlMessage || error.message)
+      }
+   }
+
+   public async getUserByEmail(email: string): Promise<User | undefined> {
+      try {
+         const result = await BaseDataBase.connection.raw(`
+            SELECT * from ${this.tableName} WHERE email = '${email}'
+         `);
+         return this.toModel(result[0][0]);
+      } catch (error) {
+         throw new Error(error.sqlMessage || error.message)
+      }
+   }
+
+   public async getUserById(id: string): Promise<User | undefined> {
+      try {
+         const result = await BaseDataBase.connection.raw(`
+            SELECT * from ${this.tableName} WHERE id = '${id}'
+         `);
+         return this.toModel(result[0][0]);
+      } catch (error) {
+         throw new Error(error.sqlMessage || error.message)
+      }
+   }
+
+   public async getAllUsers(): Promise<User[]> {
+      try {
+         const result = await BaseDataBase.connection.raw(`
+            SELECT * from ${this.tableName}
+         `);
+         return result[0].map((res: any) => {
+            return this.toModel(res);
+         });
+      } catch (error) {
+         throw new Error(error.sqlMessage || error.message)
+      }
+   }
+}
+
+export default new UserDatabase()
