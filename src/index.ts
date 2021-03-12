ea6f49fa5cb91061dfaae40174629b8dddc2b666
rafae <rafaelgf@gmail.com>
2021-03-12 15:17:35 -0300

    make it better

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ const app = express();
 var cors = require('cors')
 
 app.use(express.json());
-app.use(cors({origin:'http://localhost:3000'}))
+app.use(cors({origin: true}))
 
 app.use("/users", userRouter);
 app.use("/images", imageRouter);
